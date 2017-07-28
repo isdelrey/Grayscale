5b73c0940c6ba8fa0b4e32fb94b83ea850ac49e9
Ivo <ivosequeros@me.com>
2017-07-29 01:58:01 +0200

    verbose deployment webhook

--- services/self-deployment.js
+++ services/self-deployment.js
@@ -14,9 +14,13 @@ module.exports = class SelfDeployment {
     constructor() {
         // listen for webhook
         lib.app.post('/deployment', function(req, res) {
-            if(req.body.secret == config.self_deployment.secret)
+            console.log('Deployment notice');
+            if(req.body.secret == config.self_deployment.secret) {
+                console.log('Deployment notice verified');
+                console.log('Self-Deploying...');
                 // deploy
                 gad.deploy();
+            }
         });
 
         lib.app.listen(80, function() {
